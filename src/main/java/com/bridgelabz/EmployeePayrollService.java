7cc268ea603ea73e154eb2a91e7d21c5591f6912
sreeliptaa <sreelipta65@gmail.com>
2021-09-26 23:04:24 +0530

    Added: welcome message

---
+++ src/main/java/com/bridgelabz/EmployeePayrollService.java
@@ -0,0 +1,15 @@
+package com.bridgelabz;
+
+/**
+ * Purpose - To implement Employee PayRoll Service System
+ * @author - Sreelipta
+ * @since - 2021-09-26
+ */
+
+public class EmployeePayrollService {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee Payroll Service");
+    }
+}
+
+
